20a6567586b65fbc87ccffa39979022ce4128dee
hiromu-kon <hiro.0405soccer@icloud.com>
2021-08-08 20:42:28 +0900

    finish: weather app

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -2,9 +2,35 @@ import Weather from "./components/Weather"
 import './App.css';
 
 function App() {
+  const cities = [
+    {
+      city_name: 'Tokyo',
+      color_name: 'blue'
+    },
+    {
+      city_name: 'London',
+      color_name: 'yellow'
+    },
+    {
+      city_name: 'Paris',
+      color_name: 'green'
+    }, 
+    {
+      city_name: 'Los Angeles',
+      color_name: 'red'
+    },
+  ];
+
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <Weather />
+    <div className="min-h-screen flex justify-center items-center flex-wrap">
+      { 
+        cities.map((city,index) => 
+        <Weather 
+          key={index} 
+          city_name={city.city_name} 
+          color_name={city.color_name} 
+        />)
+      }
     </div>
   );
 }
