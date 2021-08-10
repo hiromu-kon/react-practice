8f38ab312be342e98db663a78efd615bcd90f67b
hiromu-kon <hiro.0405soccer@icloud.com>
2021-08-10 22:40:47 +0900

    add

---
+++ redux-todo-app/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const lists = useSelector((state) => state.lists);
+  const dispatch = useDispatch();
+  const doneList = (name) => {
+    dispatch({ type: "DONE_LIST", payload: name });
+  }
+  
+  return (
+    <div className="App">
+      <h1>ReduxでTodoリスト作成</h1>
+      <h2>未完了のTodoリスト</h2>
+      <ul>
+        {lists.filter((list) => list.complete === false).map((list, index) => (
+          <div key={index}>
+            {list.name}
+            <button onClick={() => doneList(list.name)}>完了</button>
+            </div>
+        ))}
+      </ul>
+      <h2>完了したTodoリスト</h2>
+      <ul>
+        {lists.filter((list) => list.complete === true).map((list, index) => (
+          <div key={index}>{list.name}</div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-todo-app/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from "redux";
+
+const initialState = {
+  lists: [
+    {
+      name: "ブログを確認",
+      complete: false
+    },
+    {
+      name: "メールの返信",
+      complete: false
+    },
+  ],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "DONE_LIST":
+      return {
+        lists: state.lists.map((list) => {
+          if (list.name !== action.payload) return list;
+
+          return {
+            ...list,
+            complete: true,
+          };
+        }),
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+export default store;
