94cd9f3d768a768c0a98b068ae77e78866ca2f63
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-12-30 21:47:25 +0900

    fix

---
+++ redux-calender-app/redux-sample/src/components/Counter/container.jsx
@@ -0,0 +1,20 @@
+import Counter from "./presentation";
+import connect from 'react-redux';
+import { increment, decrement } from "../../redux/count/actions";
+
+const mapStateProps = ({ count }) => ({ count });
+
+const mapDispatchProps = dispatch => ({
+  increment: count => {
+    dispatch(increment(count));
+  },
+  decrement: count => {
+    dispatch(decrement(count));
+  }
+});
+
+export default connect(
+  mapStateProps,
+  mapDispatchProps
+)(Counter);
+
