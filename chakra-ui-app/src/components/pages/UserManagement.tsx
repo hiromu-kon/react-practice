1ff21d000dce1324a47a5f13834c1432dbf868ab
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-01-16 12:26:10 +0900

    ユーザーモーダル作成

--- chakra-ui-app/src/components/pages/UserManagement.tsx
+++ chakra-ui-app/src/components/pages/UserManagement.tsx
@@ -1,10 +1,44 @@
-import { memo, VFC } from "react";
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), [])
+
+  const onClickUser = useCallback(() => onOpen(), [])
 
   return (
     <>
-      <p>ユーザー管理ページです</p>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+      </Wrap>
+      )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
